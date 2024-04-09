de7823177f3741247b31bbe911fbacd8edda7873
Waliyu Habeeb <Ademolawaliyu1@gmail.com>
2024-04-09 22:43:18 +0100

    adding

---
+++ Menu/Main.cs
@@ -0,0 +1,45 @@
+namespace OOP_Project.Menu
+{
+    public class Main
+    {
+        CustomerRegistration customerRegistration;
+
+        CustomerLogin customerLogin;
+
+        public Main()
+        {
+            customerRegistration = new CustomerRegistration();
+            customerLogin = new CustomerLogin();
+        }
+        public void LandingMenu()
+        {
+            System.Console.WriteLine("enter 1 to go to Customer menu \n2 to main menu");
+            int option;
+            while(!int.TryParse(Console.ReadLine(), out option))
+            {
+                Console.WriteLine("invalid option");
+            }
+            switch (option)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+                    break;
+
+            }
+
+        }
+        public void CustomerMenu()
+        {
+            System.Console.WriteLine("1. signin\n2. Sign-up");
+
+        }
+
+        public void AdminMenu()
+        {
+            
+        }
+    }
+}
